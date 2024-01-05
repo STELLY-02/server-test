25afd40762aebc2b0ae535978d721e6f5244fd43
Stelly <stellytxm@student.usm.my>
2024-01-05 18:36:14 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+// import React,{useEffect,useState} from 'react'
 
-function App() {
+// function App() {
+//   const [backendData, setBackendData] = useState([{}])
+
+//   useEffect(()=>{
+//     fetch("/api").then(
+//       response => response.json()
+//     ).then(
+//       data =>{
+//         setBackendData(data)
+//       }
+//     )
+//   },[])
+
+//   return (
+//     <div>
+      
+//     </div>
+//   )
+// }
+
+// export default App
+
+import React from 'react'
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <div>App</div>
+  )
 }
 
-export default App;
+export default App
